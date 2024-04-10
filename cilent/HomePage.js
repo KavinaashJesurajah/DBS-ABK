1773164147507f9de0aa82284d578ecc467b6287
KavinaashJesurajah <kavinaashjesurajah@gmail.com>
2024-04-10 16:49:39 -0400

    Update HomePage.js

--- cilent/HomePage.js
+++ cilent/HomePage.js
@@ -3,14 +3,14 @@ const user = {
     name: 'Test User',
     id: '123',
     university_id: '456'
-};  */
+};  
 
 // Use the mock data directly
 document.getElementById('welcomeMessage').textContent = 'Welcome ' + user.name + '!';
 document.getElementById('name').textContent = 'Name: ' + user.name;
 document.getElementById('id').textContent = 'ID: ' + user.id;
 document.getElementById('university_id').textContent = 'University ID: ' + user.university_id;
-
+*/
 
 // Replace with your actual fetch request
 fetch('http://localhost:8800/api/info', {
